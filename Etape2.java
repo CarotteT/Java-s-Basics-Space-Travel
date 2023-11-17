0a2702fbf006cf48f6d86fa4b01eb8b21baec311
Caroline <145659306+CarotteT@users.noreply.github.com>
2023-11-17 11:56:20 +0100

    Etape2.java

--- Etape2.java
+++ Etape2.java
@@ -35,3 +35,42 @@ public class SpaceTravel
          } while(true);
     }
 }
+
+
+import java.util.Scanner;
+
+public class SpaceTravel
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to the SpaceTravel agency");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What do you want to do ? [h for help]");
+      
+        
+        do
+        { 
+            String choice = scan.nextLine();  
+            char monchoix=choice.charAt(0);         
+            switch(monchoix)
+            {
+                case 'h': 
+                    System.out.println("h: print this help screen \n" + "q : quit the program");
+                    break;
+                case 'q': 
+                    System.out.println("Bye bye!");
+                    System.exit(0);
+                    break;
+                case 'l':
+                    Planet[] valuesArray = Planet.values(); // Recuperation du tableau
+                    for (int i = 0 ; i < valuesArray.length ; i++) // Parcours du tableau
+                    { 
+                        System.out.println(valuesArray[i]);
+                    }
+                default:
+                    System.out.println("Unknown command. Type h for help");
+                    break; 
+            }
+         } while(true);
+    }
+}
